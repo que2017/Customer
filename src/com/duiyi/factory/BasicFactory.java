00dd0ced9dcf134b3043cd41c11da12168c8a795
que2017 <1563645183@qq.com>
2020-03-16 23:06:14 +0800

    完成工厂类

--- src/com/duiyi/factory/BasicFactory.java
+++ src/com/duiyi/factory/BasicFactory.java
@@ -1,5 +1,41 @@
 package com.duiyi.factory;
 
-public class BasicFactory {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
+public class BasicFactory {
+	private static BasicFactory factory = new BasicFactory();
+	
+	private static Properties prop = null;
+	
+	static {
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileReader(BasicFactory.class.getClassLoader().getResource("config.properties").getPath()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private BasicFactory() {
+	}
+	
+	public static BasicFactory getFactory() {
+		return factory;
+	}
+	
+	public <T> T getInstance(Class<T> clazz) {
+		String interfaceName = clazz.getSimpleName();
+		String className = prop.getProperty(interfaceName);
+		try {
+			return (T) Class.forName(className).newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
 }
